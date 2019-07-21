6d344014e91a40659e7ffc29a280023a7b93cfe7
Benjamin Rinaldo <bkrinald@buffalo.edu>
2019-07-21 14:16:37 -0400

    Finished project

--- src/ExercisesDay03.scala
+++ src/ExercisesDay03.scala
@@ -1,9 +1,10 @@
 object ExercisesDay03 {
   def main(args: Array[String]): Unit = {
     //Exercise 1
-    println(Teams())
-    println()
+    //println(Teams())
+    //println()
     //Exercise 2
+    println(sparseTest())
   }
 
   //Exercise 1
@@ -70,25 +71,16 @@ object ExercisesDay03 {
   }
 
   //Exercise 2
-  class sparseVector(vector: Map[Int, Int]) {
-    private val v1:Map[Int,Int] = vector
-    print("How long is the vector: ")
-    private val length:Int = scala.io.StdIn.readInt()
-    if (!isSparse) {
-      println("The vector is not spars.")
-      sys.exit(0)
-    }
-
+  class sparseVector(vector: (Map[Int, Int], Int)) {
+    private val v1:Map[Int,Int] = vector._1
+    private val length:Int = vector._2
     def get_vector: Map[Int, Int] = {
       v1
     }
 
     def isSparse:Boolean = {
-      var num = 0
-      for (i<-v1){
-        num= num + 1
-      }
-      if ((num/length)*100 > 75){
+      val num = v1.size
+      if ((num/length)*100 <= 25){
         true
       }
       else{
@@ -97,6 +89,10 @@ object ExercisesDay03 {
     }
 
     def get_total: Int = {
+      if (!isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
       val values = v1.values
       var total = 0
       for (i <- values) {
@@ -105,15 +101,15 @@ object ExercisesDay03 {
       total
     }
 
-    def toSparseVector(P1: List[Int]): sparseVector = {
-        var retMap = Map[Int, Int]()
-        for (i <- 0 to P1.length if P1(i) != 0) {
-          retMap += (i -> P1(i))
-        }
-        sparseVector.this
-    }
-
     def vectorSum(other: sparseVector): sparseVector = {
+      if (!isSparse) {
+        println(s"The vector $v1 is not sparse.")
+        sys.exit(0)
+      }
+      if (!other.isSparse) {
+        println(s"The vector $other is not sparse.")
+        sys.exit(0)
+      }
       var m3 = Map[Int, Int]()
       for (i <- v1.keys) {
         if (other.get_vector.contains(i)) {
@@ -128,17 +124,26 @@ object ExercisesDay03 {
           m3 = m3 + (j -> other.get_vector(j))
         }
       }
-      sparseVector.this
+      val sv= new sparseVector((m3,m3.size))
+      sv
     }
 
     def vectorDifference(other:sparseVector): sparseVector = {
+      if (!isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
+      if (!other.isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
       var m= Map[Int,Int]()
       for (i<-v1.keys){
         if (other.get_vector.contains(i)){
-          m = m + (i -> Math.abs(v1(i) - other.get_vector(i)))
+          m = m + (i -> (v1(i) - other.get_vector(i)))
         }
         else {
-          m = m + (i -> (0-v1(i)))
+          m = m + (i -> (v1(i)-0))
         }
       }
       for (j <- other.get_vector.keys) {
@@ -146,26 +151,32 @@ object ExercisesDay03 {
           m = m + (j -> (0-other.get_vector(j)))
         }
       }
-      sparseVector.this
+      val sv= new sparseVector((m,m.size))
+      sv
     }
 
-    def vectorIntScalar(scalar: Int):sparseVector={
+    def vectorScalar(scalar: Int):sparseVector={
+      if (!isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
       var m = Map[Int, Int]()
       for (i <- v1.keys) {
         m = m + (i -> (v1(i) * scalar))
       }
-      sparseVector.this
-    }
-
-    def vectorDoubleScalar(scalar: Double):sparseVector={
-      var m = Map[Int, Double]()
-      for (i <- v1.keys) {
-        m = m + (i -> (v1(i).toDouble * scalar))
-      }
-      sparseVector.this
+      val sv= new sparseVector((m,m.size))
+      sv
     }
 
     def vectorDot(other: sparseVector):Int={
+      if (!isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
+      if (!other.isSparse) {
+        println("The vector is not sparse.")
+        sys.exit(0)
+      }
       var m3=Map[Int,Int]()
       for (i<-v1.keys) {
         if (other.get_vector.contains(i)) {
@@ -174,10 +185,43 @@ object ExercisesDay03 {
       }
       m3.values.sum
     }
+  }
 
-    def vectorCross(other:sparseVector):sparseVector={
-      
+  def toSparseVector(P1: List[Int]): sparseVector = {
+    var retMap = Map[Int, Int]()
+    for (i <- 0 until P1.length if P1(i) != 0) {
+      retMap += (i -> P1(i))
     }
+    val ret= new sparseVector((retMap,P1.length))
+    if (!ret.isSparse) {
+      println("The vector is not sparse.")
+      sys.exit(0)
+    }
+    ret
+  }
+
+  def vectors():sparseVector={
+    println("Enter a sparse vector in the form x,x,x,x,x,x,x: ")
+    val in= scala.io.StdIn.readLine()
+    val vector=in.split(',').map(x => x.toInt).toList
+    val map=toSparseVector(vector)
+    map
+  }
+  def sparseTest():String={
+    val sv1= vectors()
+    val sv2= vectors()
+    println(s"Testing get_vector method. Sparse vector 1: ${sv1.get_vector}")
+    println(s"Testing get_vector method. Sparse vector 2: ${sv2.get_vector}")
+    println(s"Testing get_total method. Sparse vector 1 total: ${sv1.get_total}")
+    println(s"Testing get_total method. Sparse vector 2 total: ${sv2.get_total}")
+    println(s"Testing vectorSum method. Sparse vector 1 + 2 sum: ${sv1.vectorSum(sv2).get_vector}")
+    println(s"Testing vectorSum method. Sparse vector 2 + 1 sum: ${sv2.vectorSum(sv1).get_vector}")
+    println(s"Testing vectorDifference method. Sparse vector 1 - 2 difference: ${sv1.vectorDifference(sv2).get_vector}")
+    println(s"Testing vectorDifference method. Sparse vector 2 - 1 difference: ${sv2.vectorDifference(sv1).get_vector}")
+    println(s"Testing vectorIntScalar method. Sparse vector 1 scaled by 2: ${sv1.vectorScalar(2).get_vector}")
+    println(s"Testing vectorDot method. Sparse vector 1 dot sparse vector 2: ${sv1.vectorDot(sv2)}")
+    println(s"Testing vectorDot method. Sparse vector 2 dot sparse vector 1: ${sv2.vectorDot(sv1)}")
+    "\n"
   }
 
 }
